5eb6314ed6b6728a4d690483087b848ef23f3edf
toneiojimz <ajimeneza77@gmail.com>
2019-12-20 08:53:29 -0800

    created login page

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
+import axiosWithAuth from '../utils/axiosWithAuth';
 import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
+const BubblePage = (props) => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+    useEffect(() => {
+
+      axiosWithAuth()
+          .get('http://localhost:5000/api/bubbles')
+          .then(res => {
+            console.log(res)
+            setColorList(res.data)
+          })
+          .catch(err => console.log(err));
+    }, []);
+    
+    console.log(bubbles);
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import bulma from 'bulma';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credential, setCredential]= useState({ username: 'Lambda School', password: 'i<3Lambd4'});
+    const [logged, setLogged]= useState(false);
+
+  const login = e => {
+      e.preventDefault();
+      setLogged(true);
+
+      axiosWithAuth()
+        .post('/login', credential)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            setLogged(false);
+            props.history.push('/bubbles');
+        })
+        .catch(err => console.log(err));
+  };
+
+  const handleChange = e => {
+    setCredential({
+        ...credential, 
+        [e.target.name]: e.target.value
+    });
+};
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+        {logged && <p>Getting Colored</p>}
+      <form className='column is-half is-centered'   onSubmit={login}>
+          <input className='input is-focused is-one-fourth is-centered '
+            type="text"
+            name="username"
+            placeholder= "username"
+            value={credential.username}
+            onChange={handleChange}
+          />
+          <input className='input is-focused is-one-fourth is-centered '
+            type="password"
+            name="password"
+            placeholder="password"
+            value={credential.password}
+            onChange={handleChange}
+          />
+          <button className='button is-success is-large'>Log in</button>
+        </form>
+    </div>
   );
 };
 
